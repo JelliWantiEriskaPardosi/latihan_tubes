6bf35383390ad2b5fca6c108c48208e09d778290
ainawanda <2510514004@mahasiswa.upnvj.ac.id>
2026-05-28 14:39:09 +0700

    ini nambahin lagi

--- lah.c
+++ lah.c
@@ -14,3 +14,17 @@ int main() {
     printf("Hasil dari banyak(%d) adalah: %d\n", input, hasil);
     return 0;
 }
+//ANJAYFEHFJHEBFHBEFHGBEFHBEHFB
+int banyak(int x) {
+    if (x < 0) {
+        return 0; // Kondisi berhenti
+    } else {
+        return x + banyak(x - 2); // Pemanggilan rekursif
+    }
+}
+
+int main() {
+    int input = 5;
+    int hasil = banyak(input);
+    printf("Hasil dari banyak(%d) adalah: %d\n", input, hasil);
+    return 0;
